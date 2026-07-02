8192f3bf7666e1244ef6986652c694c649312514
willsf2021 <willsf2021@gmail.com>
2026-07-02 16:56:42 -0300

    doc: inicializado pacote de dados_built_in

---
+++ curso-dart/lib/dados_built_in.dart
@@ -0,0 +1,7 @@
+void returnText(){
+
+  // Tipos de dados Built-In são tipos que veem instalados automaticamente junto com Dart quando instalamos, sendo:
+  // num,int.double,string,boolean,list,ser,map,runes,null,object,future,stream,iterable,never,dynamic,void
+
+  print("Olá");
+}
