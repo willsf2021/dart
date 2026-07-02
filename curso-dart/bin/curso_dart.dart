19b4e407dc2c8d86f38b528b9a19a85cd248a946
willsf2021 <willsf2021@gmail.com>
2026-07-02 20:28:08 -0300

    doc: package de dados built-in

--- curso-dart/bin/curso_dart.dart
+++ curso-dart/bin/curso_dart.dart
@@ -2,10 +2,12 @@ import 'package:curso_dart/nullable.dart' as nullable;
 import 'package:curso_dart/variaveis_base.dart' as variaveis_base;
 import 'package:curso_dart/dynamic_var.dart' as dynamic_var;
 import 'package:curso_dart/final_const.dart' as final_const;
+import 'package:curso_dart/dados_built_in.dart' as dados_built_in;
 
 void main(List<String> arguments) {
   variaveis_base.returnText();
   dynamic_var.returnText();
   final_const.returnText();
+  dados_built_in.returnText();
   nullable.returnText();
 }
--- curso-dart/lib/dados_built_in.dart
+++ curso-dart/lib/dados_built_in.dart
@@ -1,7 +1,18 @@
-void returnText(){
-
+void returnText() {
   // Tipos de dados Built-In são tipos que veem instalados automaticamente junto com Dart quando instalamos, sendo:
   // num,int.double,string,boolean,list,ser,map,runes,null,object,future,stream,iterable,never,dynamic,void
 
-  print("Olá");
+  print("------------------------------------------------Dados Built-In");
+  num numero = 2; // Contém int e double, pode ser ambos
+  print(numero);
+  // Coleções de Dados
+  List lista = [2, 453, 2];
+  Set sett = {1, 2, 3};
+  Map mappa = {"nome": "Wilson", "Sobrenome": "Pereira Júnior"};
+  Runes runa = new Runes('\u{1f605}');
+
+  print(sett);
+  print(lista);
+  print(mappa);
+  print(new String.fromCharCodes(runa));
 }
