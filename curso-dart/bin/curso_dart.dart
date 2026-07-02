4b4bff8a739dc0935c118ca4ef4e610a5f477167
willsf2021 <willsf2021@gmail.com>
2026-07-02 15:56:35 -0300

    doc: faz anotações sobre diferenças entre final e const

--- curso-dart/bin/curso_dart.dart
+++ curso-dart/bin/curso_dart.dart
@@ -50,5 +50,33 @@ var:
 - É uma forma de pedir ao compilador que infira o tipo da variável a partir do seu valor inicial.
   */
 
+ /*
+ Final e Const
+ */
+
+  // Significa que já é inicializada em seu ESTADO FINAL, OU, caso declarada somente, a sua primeira atribuição será o seu ESTADO FINAL.
+  final int teste;
+  teste = 3;
+
+  // Significa que DEVE SER INICIALIZADA, ou seja, quando declarada deve ter seu valor atribuído.
+  const teste2 = 3;
+
+  // Ambas fazem inferência de tipo, no entanto final é recomendado TIPAR na declaração ou inicialização:
+  print(teste.runtimeType);
+  print(teste2.runtimeType);
+
+
+  final lista = [3,4,5];
+  lista.add(7);
+  lista.add(8);
+  lista.add(9);
+  // Em final list posso modificar o valor, mas não reatribuir.
+
+  const lista2 = [3,5,7];
+  // lista2.add(234);
+  // Em const list não posso modificar nem reatribuir.
+  print(lista);
+  print(lista2);
+
   print('Hello world: ${curso_dart.calculate()}!');
 }
